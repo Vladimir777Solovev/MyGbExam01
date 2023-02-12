5b561bd43023861cfd22df73acbf86da5d4164fc
Your Name <you@example.com>
2023-02-12 20:49:26 +0300

    Task is complete

--- FinalExam/Program.cs
+++ FinalExam/Program.cs
@@ -6,11 +6,15 @@
 // лучше обойтись исключительно массивами
 
 Console.WriteLine("Input size of elements in massive (not more then 3): ");
+
 // Узнаем у пользователя количество строк или чисел в массиве
 int size  = int.Parse(Console.ReadLine());
+
 // Задаем массив
+
 string[] Array1 = new string[size];
 // Вводим цикл со счетчиком
+
 for (int i = 0; i < size; i++)
 {
 	Console.Write("Input an elements of massive (by pushing enter): ");
@@ -19,3 +23,28 @@ for (int i = 0; i < size; i++)
 }
 // Выводим массив заданный пользователем
 Console.WriteLine($"Your massive is {string.Join(",", Array1)}");
+
+int count = 0;
+int max = 3;
+
+// Вводим счетчик подходящих элементов
+for (int i = 0; i < Array1.Length; i++)
+{
+	if(Array1[i].Length <= max){
+		count++;
+	}   
+}
+Console.WriteLine();
+Console.Write("Resulting massive: [");
+
+// Вводим массив выдающий конечный результат
+string[] Array2 = new string[count];
+int j = 0;
+for (int i = 0; i < Array1.Length; i++){
+    if(Array1[i].Length <= max){
+        Array2[j] = Array1[i];
+        Console.Write(Array2[j] + ",");
+        j++;
+    }
+}
+Console.Write("]");
